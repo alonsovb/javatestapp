d7c4966a992d77d154913d8d964407cd60973510
= <=>
2013-11-05 15:51:20 -0600

    Added conditional

--- src/hello.java
+++ src/hello.java
@@ -2,7 +2,7 @@ class Hello {
     public static void main(String args[]){
         System.out.println("Starting to count:");
 		for (int i = 0; i < 5; i++) {
-			System.out.println(String.valueOf(i));
+			System.out.println(String.valueOf(i) + ((i % 2 == 0) ? " is even" : " is odd" ));
 		}
 		System.out.println("We're good to go.");
     }
