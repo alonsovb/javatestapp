b6aa17e417122a2697a300e0783b88aa6ee337dd
= <=>
2013-11-05 15:48:59 -0600

    Added for loop in src file

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,9 @@
 class Hello {
     public static void main(String args[]){
-        System.out.println("Hello World!");
-	System.out.println("Nice going.");
+        System.out.println("Starting to count:");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(String.valueOf(i))
+		}
+		System.out.println("We're good to go.");
     }
 }
